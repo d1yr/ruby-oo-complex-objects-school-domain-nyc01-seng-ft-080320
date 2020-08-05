1f9504fbbcf576cb879241b21d39284d4d7a1f9c
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-08-05 03:56:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+class School
+  attr_accessor :school, :roster,
+def initialize(school)
+  @school = school
+  @roster = {}
+end
+def add_student(student, grade)
+  roster[grade] ||=[]
+  roster[grade]<< student
+end
+#def grade(num)
+ # roster.detect do |x,y|
+  #  if x == num
+   #   return y
+   # end
+#  end
+#end
+
+end
